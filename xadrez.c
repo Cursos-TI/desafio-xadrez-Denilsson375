27f681af360b8627832d459c345b3e1a15827e43
Denilsson375 <denilssonborges@gmail.com>
2026-06-07 14:04:56 +0000

    correção do código e adição do movimento do bispo

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 int main(){
+    int torre = 0;
+    int bispo = 0;
+    int rainha = 0;
 
-    // Movimentação da Torre.
-    for( int torre = 0; torre < 5; torre++) {
+// Movimentação da Torre.
+    for(int bispo = 0; torre < 5; torre++) {
         printf("Direita\n");
     }
+
+// Movimentação da Torre.
+
+    while (bispo < 5 ) {
+        printf("Cima, Direita\n");
+         bispo++;
+    }
+    
 
 }
